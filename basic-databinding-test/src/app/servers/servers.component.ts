150a4679b94f0f8e7fccc21189aad816320efeb1
jeswinmoses1997 <jeswinmoses97@gmail.com>
2024-02-26 13:05:20 +0530

    completed till ngcolor and ngstyle

--- basic-databinding-test/src/app/servers/servers.component.ts
+++ basic-databinding-test/src/app/servers/servers.component.ts
@@ -8,24 +8,25 @@ import { Component, OnInit } from '@angular/core';
   //   <app-server></app-server>
   //   <app-server></app-server>`,
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Testserver';
-
+  serverCreated = false;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.serverCreated = true;
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {
