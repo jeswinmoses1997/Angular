37cfd31b214a8f389650dd3355ce378359c63385
jeswinmoses1997 <jeswinmoses97@gmail.com>
2024-04-27 10:30:58 +0530

    completed 14/34

--- jes-angular-project/src/app/shopping-list/shopping-list.component.ts
+++ jes-angular-project/src/app/shopping-list/shopping-list.component.ts
@@ -11,13 +11,13 @@ import { Subscription } from 'rxjs';
 })
 export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
-  private igChangeSub: Subscription;
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) {}
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged.subscribe(
+    this.subscription = this.slService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
@@ -25,6 +25,6 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.igChangeSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 }
