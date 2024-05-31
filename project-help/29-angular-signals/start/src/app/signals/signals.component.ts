86104e64225cda4140cd737cb101c5cf3fcb0a5e
jeswinmoses1997 <jeswinmoses97@gmail.com>
2024-05-31 12:03:29 +0530

    completed 29/38

---
+++ project-help/29-angular-signals/start/src/app/signals/signals.component.ts
@@ -0,0 +1,33 @@
+import { NgFor } from '@angular/common';
+import { Component, computed, effect, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  templateUrl: './signals.component.html',
+  standalone: true,
+  imports: [NgFor],
+})
+export class SignalsComponent {
+  actions = signal<string[]>([]);
+  counter = signal(0);
+  doubleCounter = computed(() => this.counter() * 2);
+
+  constructor() {
+    effect(() => console.log(this.counter()));
+  }
+
+  increment() {
+    // this.counter.update((oldCounter) => oldCounter + 1);
+    this.counter.set(this.counter() + 1);
+    // this.counter.mutate();
+    this.actions.update((oldActions) => [...oldActions, 'INCREMENT']);
+    // this.actions.mutate((oldActions) => oldActions.push('INCREMENT'));
+    // this.actions.push('INCREMENT');
+  }
+
+  decrement() {
+    this.counter.update((oldCounter) => oldCounter - 1);
+    // this.actions.push('DECREMENT');
+    this.actions.update((oldActions) => [...oldActions, 'DECREMENT']);
+  }
+}
